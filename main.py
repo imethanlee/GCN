3f7f3739d5cfb3821301617c970fad875b019e51
Ethan Lee <Howisitgoing@yeah.net>
2020-10-12 21:07:03 +0800

    almost finished.

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ import torch.nn.functional as F
 import torch.nn.init as init
 import torch.optim as optim
 
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
 
 # 用于保存处理好的数据
 Data = namedtuple('Data', ['x', 'y', 'adjacency', 'train_mask', 'val_mask', 'test_mask'])
@@ -23,7 +26,7 @@ class CoraData(object):
     filenames = ["ind.cora.{}".format(name)
                  for name in ['x', 'tx', 'allx', 'y', 'ty', 'ally', 'graph', 'test.index']]
 
-    def __init__(self, data_root = "cora", rebuild=False):
+    def __init__(self, data_root="cora", rebuild=False):
         """Cora数据，包括数据下载，处理，加载等功能
         当数据的缓存文件存在时，将使用缓存文件，否则将下载、进行处理，并缓存到磁盘
         处理之后的数据可以通过属性 .data 获得，它将返回一个数据对象，包括如下几部分：
@@ -63,6 +66,7 @@ class CoraData(object):
         for name in self.filenames:
             if not osp.exists(osp.join(save_path, name)):
                 self.download_data("{}/{}".format(self.download_url, name), save_path)
+
     @staticmethod
     def download_data(url, save_path):
         """数据下载工具，当原始数据不存在时将会进行下载"""
@@ -103,7 +107,7 @@ class CoraData(object):
         val_mask[val_index] = True
         test_mask[test_index] = True
 
-        adjancency = self.build_adjacency(graph)
+        adjacency = self.build_adjacency(graph)
         print("Node's feature shape: ", x.shape)
         print("Node's label shape: ", y.shape)
         print("Adjacency's shape: ", adjacency.shape)
@@ -111,7 +115,7 @@ class CoraData(object):
         print("Number of validation nodes: ", val_mask.sum())
         print("Number of test nodes: ", test_mask.sum())
 
-        return Data(x=x, y=y, adjacency=adjancency,
+        return Data(x=x, y=y, adjacency=adjacency,
                     train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
 
     @staticmethod
@@ -125,8 +129,8 @@ class CoraData(object):
         # 去除重复的边
         edge_index = list(k for k, _ in itertools.groupby(sorted(edge_index)))
         edge_index = np.asarray(edge_index)
-        adjacency = sp.coo_matrix((np.ones(len(edge_index))),
-                                  (edge_index[:, 0], edge_index[:,1]),
+        adjacency = sp.coo_matrix((np.ones(len(edge_index)),
+                                  (edge_index[:, 0], edge_index[:, 1])),
                                   shape=(num_nodes, num_nodes), dtype="float32")
         return adjacency
 
@@ -141,3 +145,169 @@ class CoraData(object):
             out = pickle.load(open(path, "rb"), encoding="latin1")
             out = out.toarray() if hasattr(out, "toarray") else out
             return out
+
+
+class GraphConvolution(nn.Module):
+    def __init__(self, input_dim, output_dim, use_bias=True):
+        """图卷积：L*X*\theta
+        Args:
+        ----------
+            input_dim: int
+                节点输入特征的维度
+            output_dim: int
+                输出特征维度
+            use_bias : bool, optional
+                是否使用偏置
+        """
+        super(GraphConvolution, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.Tensor(output_dim))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight)
+        if self.use_bias:
+            init.zeros_(self.bias)
+
+    def forward(self, adjacency, input_feature):
+        """邻接矩阵是稀疏矩阵，因此在计算时使用稀疏矩阵乘法
+
+        Args:
+        -------
+            adjacency: torch.sparse.FloatTensor
+                邻接矩阵
+            input_feature: torch.Tensor
+                输入特征
+        """
+        support = torch.mm(input_feature, self.weight)
+        output = torch.sparse.mm(adjacency, support)
+        if self.use_bias:
+            output += self.bias
+        return output
+
+
+class GcnNet(nn.Module):
+    """
+    定义一个包含两层GraphConvolution的模型
+    """
+    def __init__(self, input_dim=1433):
+        super(GcnNet, self).__init__()
+        self.gcn1 = GraphConvolution(input_dim, 16)
+        self.gcn2 = GraphConvolution(16, 7)
+
+    def forward(self, adjacency, feature):
+        h = F.relu(self.gcn1(adjacency, feature))
+        logits = self.gcn2(adjacency, h)
+        return logits
+
+
+def normalization(adjacency):
+    """计算L=D^-0.5 * (A+I) * D^-0.5"""
+    adjacency += sp.eye(adjacency.shape[0]) #增加自连接
+    degree = np.array(adjacency.sum(1))
+    d_hat = sp.diags(np.power(degree, -0.5).flatten())
+    return d_hat.dot(adjacency).dot(d_hat).tocoo()
+
+
+# 超参数定义
+learning_rate = 0.1
+weight_decay = 5e-4
+epochs = 200
+# 模型定义，包括模型实例化、损失函数与优化器定义
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = GcnNet().to(device)
+# 损失函数使用交叉熵
+criterion = nn.CrossEntropyLoss().to(device)
+# 优化器使用Adam
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+
+# 加载数据，并转化为torch.Tensor
+dataset = CoraData().data
+x = dataset.x / dataset.x.sum(1, keepdims=True) # 归一化数据，使得每一行和为1
+tensor_x = torch.from_numpy(x).to(device)
+tensor_y = torch.from_numpy(dataset.y).to(device)
+tensor_train_mask = torch.from_numpy(dataset.train_mask).to(device)
+tensor_val_mask = torch.from_numpy(dataset.val_mask).to(device)
+tensor_test_mask = torch.from_numpy(dataset.test_mask).to(device)
+normalize_adjacency = normalization(dataset.adjacency) # 规范化邻接矩阵
+indices = torch.from_numpy(
+    np.asarray([normalize_adjacency.row,
+                normalize_adjacency.col]).astype('int64')).long()
+values = torch.from_numpy(normalize_adjacency.data.astype(np.float32))
+tensor_adjacency = torch.sparse.FloatTensor(indices, values, (2708, 2708)).to(device)
+
+
+def train():
+    loss_history = []
+    val_acc_history = []
+    model.train()
+    train_y = tensor_y[tensor_train_mask]
+    for epoch in range(epochs):
+        logits = model(tensor_adjacency, tensor_x) # 前向传播
+        train_mask_logits = logits[tensor_train_mask] # 只选择训练节点进行监督
+        loss = criterion(train_mask_logits, train_y) # 计算损失值
+        optimizer.zero_grad()
+        loss.backward() # 反向传播计算参数的梯度
+        optimizer.step() # 使用优化方法进行梯度更新
+        train_acc, _, _ = test(tensor_train_mask) # 计算当前模型在训练集上的准确率
+        val_acc, _, _ = test(tensor_val_mask) # 计算当前模型在验证集上的准确率
+        # 记录训练过程中损失值和准确率的变化，用于画图
+        loss_history.append(loss.item())
+        val_acc_history.append(val_acc.item())
+        print("Epoch {:03d}: Loss {:.4f}, TrainAcc {:.4}, ValAcc {:.4f}".format(
+            epoch, loss.item(), train_acc.item(), val_acc.item()))
+    return loss_history, val_acc_history
+
+
+def test(mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(tensor_adjacency, tensor_x)
+        test_mask_logits = logits[mask]
+        predict_y = test_mask_logits.max(1)[1]
+        accuracy = torch.eq(predict_y, tensor_y[mask]).float().mean()
+    return accuracy, test_mask_logits.cpu().numpy(), tensor_y[mask].cpu().numpy()
+
+
+def plot_loss_with_acc(loss_history, val_acc_history):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(range(len(loss_history)), loss_history,
+             c=np.array([255, 71, 90]) / 255.)
+    plt.ylabel('Loss')
+
+    ax2 = fig.add_subplot(111, sharex=ax1, frameon=False)
+    ax2.plot(range(len(val_acc_history)), val_acc_history,
+             c=np.array([79, 179, 255]) / 255.)
+    ax2.yaxis.tick_right()
+    ax2.yaxis.set_label_position("right")
+    plt.ylabel('ValAcc')
+
+    plt.xlabel('Epoch')
+    plt.title('Training Loss & Validation Accuracy')
+    plt.show()
+
+
+loss, val_acc = train()
+test_acc, test_logits, test_label = test(tensor_test_mask)
+print("Test accuracy: ", test_acc.item())
+
+plot_loss_with_acc(loss, val_acc)
+
+
+tsne = TSNE()
+out = tsne.fit_transform(test_logits)
+fig = plt.figure()
+for i in range(7):
+    indices = test_label == i
+    x, y = out[indices].T
+    plt.scatter(x, y, label=str(i))
+plt.legend()
+plt.show()
